c02b085d5a0ac4d588bbb7b7bd942d52e1842eee
bufgix <ooruc471@yandex.com>
2021-05-16 21:50:25 +0300

    fix: change component name

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 
+
 type Props = {
   className: string;
   placeholder: string;
   rows: number;
 };
 
-export default function textArea({
-  className,
-  rows,
-  placeholder,
-}: Props) {
+export default function textArea({ className, rows, placeholder }: Props) {
   return (
     <div>
       <textarea
